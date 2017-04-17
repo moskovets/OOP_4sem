348a2466edeab53171d65232ebe8b43c3f44df9c
moskovets <moskovets2009@yandex.ru>
2017-04-17 21:51:40 +0300

    add BaseException class

--- laba2/cexception.h
+++ laba2/cexception.h
@@ -1,11 +1,45 @@
 #ifndef CEXCEPTION_H
 #define CEXCEPTION_H
 #include <exception>
+#include <cstring>
+#include <string>
 
-class CException
+class CBaseException : public std::exception
 {
+
 public:
-    CException();
+
+    const char *what()
+    {
+        return msg;
+    }
+
+    explicit CBaseException() = default;
+
+    explicit CBaseException(char *error)
+    {
+        int n = strlen(error);
+        msg = new char[n];
+        if(!msg) { return; } //???????????????
+        strcpy(msg, error);
+    }
+
+    explicit CBaseException(std::string &error)
+    {
+        int n = error.size();
+        msg = new char[n];
+        if(!msg) { return; } //???????????????
+        strcpy(msg, error.c_str());
+    }
+
+    ~CBaseException()
+    {
+        delete[] msg;
+    }
+
+private:
+
+    char *msg = nullptr;
 };
 
 #endif // CEXCEPTION_H
